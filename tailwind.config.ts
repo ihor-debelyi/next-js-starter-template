e3d9712c7ea24c9ad81b41a967daa55b3cb49922
Ihor Debelyi <ihor.debelyi@gmail.com>
2024-11-18 21:20:33 +0100

    added template

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import { nextui } from "@nextui-org/react";
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
+  },
+  plugins: [require("tailwindcss-animate"), nextui()],
+} satisfies Config;
